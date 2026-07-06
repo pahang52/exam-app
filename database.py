37e4bdcfb3942d84f2214f868181624440e9b2f3
pahang52 <aslam.pahang52@gmail.com>
2026-07-06 21:01:35 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,4 @@
 import sqlite3
-import random
-import os
 
 DB_NAME = "questions.db"
 
@@ -25,272 +23,104 @@ class Database:
             answer INTEGER NOT NULL
         )
         """)
-
         self.conn.commit()
 
-    def add_question(
-        self,
-        lesson,
-        question,
-        option1,
-        option2,
-        option3,
-        option4,
-        answer
-    ):
-
+    def add_question(self, lesson, question, option1, option2, option3, option4, answer):
         self.cur.execute("""
-        INSERT INTO questions
-        (
-        lesson,
-        question,
-        option1,
-        option2,
-        option3,
-        option4,
-        answer
-        )
-        VALUES
-        (?,?,?,?,?,?,?)
-        """,
-        (
-            lesson,
-            question,
-            option1,
-            option2,
-            option3,
-            option4,
-            answer
-        ))
-
+        INSERT INTO questions (
+            lesson, question, option1, option2, option3, option4, answer
+        ) VALUES (?,?,?,?,?,?,?)
+        """, (lesson, question, option1, option2, option3, option4, answer))
         self.conn.commit()
 
     def close(self):
         self.conn.close()
-       def get_all_questions(self):
-        """
-        دریافت همه سوالات
-        """
-        self.cur.execute("""
-            SELECT *
-            FROM questions
-            ORDER BY id DESC
-        """)
+
+    # ---------------- GET ----------------
+
+    def get_all_questions(self):
+        self.cur.execute("SELECT * FROM questions ORDER BY id DESC")
         return self.cur.fetchall()
 
-
     def get_questions_by_lesson(self, lesson):
-        """
-        دریافت سوالات یک درس
-        """
         self.cur.execute("""
-            SELECT *
-            FROM questions
-            WHERE lesson=?
-            ORDER BY RANDOM()
+        SELECT * FROM questions
+        WHERE lesson=?
+        ORDER BY RANDOM()
         """, (lesson,))
         return self.cur.fetchall()
 
-
     def get_question(self, qid):
-        """
-        دریافت یک سوال با شناسه
-        """
-        self.cur.execute("""
-            SELECT *
-            FROM questions
-            WHERE id=?
-        """, (qid,))
+        self.cur.execute("SELECT * FROM questions WHERE id=?", (qid,))
         return self.cur.fetchone()
 
-
     def search(self, text):
-        """
-        جستجوی سوال
-        """
         self.cur.execute("""
-            SELECT *
-            FROM questions
-            WHERE question LIKE ?
-            ORDER BY id DESC
+        SELECT * FROM questions
+        WHERE question LIKE ?
+        ORDER BY id DESC
         """, (f"%{text}%",))
         return self.cur.fetchall()
 
-
     def count(self):
-        """
-        تعداد کل سوالات
-        """
-        self.cur.execute("""
-            SELECT COUNT(*)
-            FROM questions
-        """)
-        return self.cur.fetchone()[0] 
-       def update_question(
-        self,
-        qid,
-        lesson,
-        question,
-        option1,
-        option2,
-        option3,
-        option4,
-        answer
-    ):
-        """
-        ویرایش سوال
-        """
+        self.cur.execute("SELECT COUNT(*) FROM questions")
+        return self.cur.fetchone()[0]
 
+    # ---------------- UPDATE / DELETE ----------------
+
+    def update_question(self, qid, lesson, question, option1, option2, option3, option4, answer):
         self.cur.execute("""
-        UPDATE questions
-        SET
-            lesson=?,
-            question=?,
-            option1=?,
-            option2=?,
-            option3=?,
-            option4=?,
-            answer=?
+        UPDATE questions SET
+            lesson=?, question=?, option1=?, option2=?, option3=?, option4=?, answer=?
         WHERE id=?
-        """,
-        (
-            lesson,
-            question,
-            option1,
-            option2,
-            option3,
-            option4,
-            answer,
-            qid
-        ))
-
+        """, (lesson, question, option1, option2, option3, option4, answer, qid))
         self.conn.commit()
-
 
     def delete_question(self, qid):
-        """
-        حذف سوال
-        """
-
-        self.cur.execute("""
-        DELETE FROM questions
-        WHERE id=?
-        """, (qid,))
-
+        self.cur.execute("DELETE FROM questions WHERE id=?", (qid,))
         self.conn.commit()
 
+    # ---------------- RANDOM ----------------
 
     def random_questions(self, lesson, count):
-        """
-        انتخاب تصادفی سوال
-        """
-
         self.cur.execute("""
-        SELECT *
-        FROM questions
+        SELECT * FROM questions
         WHERE lesson=?
         ORDER BY RANDOM()
         LIMIT ?
-        """,
-        (
-            lesson,
-            count
-        ))
-
+        """, (lesson, count))
         return self.cur.fetchall()
-
 
     def random_by_type(self, lesson, count):
-        """
-        تابع کمکی برای آزمون ساز
-        """
-
-        return self.random_questions(
-            lesson,
-            count
-        )
-
+        return self.random_questions(lesson, count)
 
     def exists(self, qid):
-
-        self.cur.execute("""
-        SELECT COUNT(*)
-        FROM questions
-        WHERE id=?
-        """, (qid,))
-
+        self.cur.execute("SELECT COUNT(*) FROM questions WHERE id=?", (qid,))
         return self.cur.fetchone()[0] > 0
-         def get_lessons(self):
-        """
-        دریافت لیست درس‌ها
-        """
 
-        self.cur.execute("""
-        SELECT DISTINCT lesson
-        FROM questions
-        ORDER BY lesson
-        """)
+    # ---------------- EXTRA ----------------
 
+    def get_lessons(self):
+        self.cur.execute("SELECT DISTINCT lesson FROM questions ORDER BY lesson")
         return [row[0] for row in self.cur.fetchall()]
 
-
     def clear_database(self):
-        """
-        حذف تمام سوالات
-        """
-
-        self.cur.execute("""
-        DELETE FROM questions
-        """)
-
+        self.cur.execute("DELETE FROM questions")
         self.conn.commit()
-
-
-    def import_questions(self, data):
-        """
-        ورود گروهی سوالات
-        data = [
-            (
-                lesson,
-                question,
-                option1,
-                option2,
-                option3,
-                option4,
-                answer
-            )
-        ]
-        """
-
-        self.cur.executemany("""
-        INSERT INTO questions(
-            lesson,
-            question,
-            option1,
-            option2,
-            option3,
-            option4,
-            answer
-        )
-        VALUES(?,?,?,?,?,?,?)
-        """, data)
-
-        self.conn.commit()
-
 
     def export_questions(self):
-
-        self.cur.execute("""
-        SELECT *
-        FROM questions
-        ORDER BY lesson,id
-        """)
-
+        self.cur.execute("SELECT * FROM questions ORDER BY lesson, id")
         return self.cur.fetchall()
 
+    def import_questions(self, data):
+        self.cur.executemany("""
+        INSERT INTO questions(
+            lesson, question, option1, option2, option3, option4, answer
+        ) VALUES (?,?,?,?,?,?,?)
+        """, data)
+        self.conn.commit()
 
     def __del__(self):
-
         try:
             self.conn.close()
         except:
