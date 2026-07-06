1e49270772fa1b899b519f67740223fe4b374141
Parsa <parsa@example.com>
2026-07-06 13:35:04 +0330

    database part2

--- database.py
+++ database.py
@@ -67,4 +67,62 @@ class Database:
 
     def close(self):
         self.conn.close()
-    
+       def get_all_questions(self):
+        """
+        دریافت همه سوالات
+        """
+        self.cur.execute("""
+            SELECT *
+            FROM questions
+            ORDER BY id DESC
+        """)
+        return self.cur.fetchall()
+
+
+    def get_questions_by_lesson(self, lesson):
+        """
+        دریافت سوالات یک درس
+        """
+        self.cur.execute("""
+            SELECT *
+            FROM questions
+            WHERE lesson=?
+            ORDER BY RANDOM()
+        """, (lesson,))
+        return self.cur.fetchall()
+
+
+    def get_question(self, qid):
+        """
+        دریافت یک سوال با شناسه
+        """
+        self.cur.execute("""
+            SELECT *
+            FROM questions
+            WHERE id=?
+        """, (qid,))
+        return self.cur.fetchone()
+
+
+    def search(self, text):
+        """
+        جستجوی سوال
+        """
+        self.cur.execute("""
+            SELECT *
+            FROM questions
+            WHERE question LIKE ?
+            ORDER BY id DESC
+        """, (f"%{text}%",))
+        return self.cur.fetchall()
+
+
+    def count(self):
+        """
+        تعداد کل سوالات
+        """
+        self.cur.execute("""
+            SELECT COUNT(*)
+            FROM questions
+        """)
+        return self.cur.fetchone()[0] 
